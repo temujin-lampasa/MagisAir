a4845a20882f91cfda4330519bf053282c6ad22e
Temujin U. Lampasa <temujin.lampasa@obf.ateneo.edu>
2020-10-20 14:18:22 +0800

    Add only relevant models

--- src/passenger_view/admin.py
+++ src/passenger_view/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
 # Register your models here.
+from passenger_view.models import (
+    Addon,
+    Airport,
+    Booking,
+    # BookingAddonMap,
+    Crew,
+    # CrewFlightMap,
+    Flight,
+    FlightRoute,
+    Itinerary,
+    Passenger,
+)
+
+
+models = (
+    Addon,
+    Airport,
+    Booking,
+    # BookingAddonMap,
+    Crew,
+    # CrewFlightMap,
+    Flight,
+    FlightRoute,
+    Itinerary,
+    Passenger,
+)
+
+for model in models:
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
