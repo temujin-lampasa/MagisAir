c93777bf394a1cdbfd4b096275cfaab754bf2bae
Temujin U. Lampasa <temujin.lampasa@obf.ateneo.edu>
2020-10-20 19:46:22 +0800

    Add validation check for past dates

--- src/passenger_view/forms.py
+++ src/passenger_view/forms.py
@@ -1,6 +1,6 @@
 from django import forms
-
 from django.db import connection
+import datetime
 
 
 class FlightSearchForm(forms.Form):
@@ -19,5 +19,14 @@ class FlightSearchForm(forms.Form):
     def clean(self):
         cleaned_data = super().clean()
         print(cleaned_data)
-        if cleaned_data.get('from_city') == cleaned_data.get('to_city'):
+
+        to_city_val = cleaned_data.get('to_city')
+        from_city_val = cleaned_data.get('from_city')
+        date_val = cleaned_data.get('date')
+        # Check if cities are the same
+        if from_city_val == to_city_val:
             raise forms.ValidationError('Cities cannot be the same.')
+
+        # Check if date is from the past
+        if datetime.date.today() > date_val:
+            raise forms.ValidationError('Date must not be in the past.')
