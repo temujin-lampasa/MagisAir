e002208e7932ed6e664285702187c37bbf6e2583
Temujin U. Lampasa <temujin.lampasa@obf.ateneo.edu>
2020-10-21 17:11:24 +0800

    Convert flight_select_view function to class-based view

--- src/passenger_view/urls.py
+++ src/passenger_view/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 from .views import (
     home_view,
-    flight_select_view,
-    pass_info_view
+    pass_info_view,
+    FlightSelectView
                     )
 
 
 app_name = 'passenger_view'
 urlpatterns = [
     path('', home_view, name='pass_home'),
-    path('flights/', flight_select_view, name='pass_flights'),
+    path('flights/', FlightSelectView.as_view(), name='pass_flights'),
     # path('flights/<id:pk>'),
     path('pass_info/', pass_info_view, name='pass_info')
 ]
--- src/passenger_view/views.py
+++ src/passenger_view/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, reverse
 from django.http import HttpResponseRedirect
 from django.db import connection
+from django.views import View
 
 from .queries import QueryList
 from .forms import (
@@ -21,18 +22,47 @@ def home_view(request, *args, **kwargs):
     return render(request, 'passenger_view/home_view.html', context)
 
 
-def flight_select_view(request, *args, **kwargs):
-    flight_dep_date = request.session.get('flight_dep_date')
-    origin_city = request.session.get('from_city')
-    destination_city = request.session.get('to_city')
-    flights = QueryList.flight_select_query(flight_dep_date,
-                                            origin_city,
-                                            destination_city)
-    context = {'object_list': flights}
-    return render(request, 'passenger_view/flight_select.html', context)
+# def flight_select_view(request, *args, **kwargs):
+#
+#     # Validate
+#     checkbox_name = 'flight_choice'
+#     print(f"POST list: {request.POST.getlist(checkbox_name)}")
+#
+#     flight_dep_date = request.session.get('flight_dep_date')
+#     origin_city = request.session.get('from_city')
+#     destination_city = request.session.get('to_city')
+#     flights = QueryList.flight_select_query(flight_dep_date,
+#                                             origin_city,
+#                                             destination_city)
+#     context = {'object_list': flights}
+#     return render(request, 'passenger_view/flight_select.html', context)
+
+
+class FlightSelectView(View):
+    template_name = 'passenger_view/flight_select.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        # Set variables to be used:
+        flight_dep_date = request.session.get('flight_dep_date')
+        origin_city = request.session.get('from_city')
+        destination_city = request.session.get('to_city')
+        self.flights = QueryList.flight_select_query(flight_dep_date,
+                                                     origin_city,
+                                                     destination_city)
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        context = {'object_list': self.flights}
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        # Validate the form data.
+        context = {'object_list': self.flights}
+        return render(request, self.template_name, context)
 
 
 def pass_info_view(request, *args, **kwargs):
-    form = PassengerInfoForm()
+    form = PassengerInfoForm(request.POST or None)
+
     context = {"form": form}
     return render(request, 'passenger_view/pass_info.html', context)
