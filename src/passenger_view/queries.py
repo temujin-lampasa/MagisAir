0f901fc11df7e22880fcb5b941d55f9064c820b5
Temujin U. Lampasa <temujin.lampasa@obf.ateneo.edu>
2020-10-22 22:34:11 +0800

    Make queriies safe and add addon_select_query

--- src/passenger_view/queries.py
+++ src/passenger_view/queries.py
@@ -4,6 +4,18 @@ from django.db import connection
 class QueryList:
     """Queries for the database."""
 
+    def addon_select_query(addon_ids):
+        addon_ids = (1, 2, 3)
+        q = """
+        SELECT addon_description FROM addon WHERE addon_id in (%s, %s, %s);
+        """
+        cursor = connection.cursor()
+        cursor.execute(q, addon_ids)
+        res = cursor.fetchall()
+        res = [r[0] for r in res]
+        cursor.close()
+        return res
+
     def city_country_select():
         """Return list of 2-tuples (city, country)"""
         q = """
@@ -19,7 +31,7 @@ class QueryList:
     def flight_select_query(dep_date, origin, destination):
         """Return list of FlightRows"""
         flight_dep_date = dep_date
-        q = f"""SELECT
+        q = """SELECT
           a.flight_code AS "Flight",
           a.airport_city || ' (' || a.airport_country || ')' AS "Origin",
           b.airport_city || ' (' || b.airport_country || ')' AS "Destination",
@@ -33,18 +45,20 @@ class QueryList:
             FROM flight f
             JOIN flight_route fr
             ON (f.route_id=fr.route_id)
-            WHERE f.flight_dep_date = '{flight_dep_date}') a
+            WHERE f.flight_dep_date = %s) a
           JOIN
             airport b
           ON (a.airport_origin = b.airport_id)) a
         JOIN
           airport b
         ON (a.airport_destination=b.airport_id)
-        WHERE a.airport_city = '{origin}' AND b.airport_city='{destination}';
+        WHERE a.airport_city = %s AND b.airport_city=%s;
             """
 
+        vars = [flight_dep_date, origin, destination]
+
         cursor = connection.cursor()
-        cursor.execute(q)
+        cursor.execute(q, vars)
         flights = cursor.fetchall()
         cursor.close()
         flight_rows = [FlightRow(f) for f in flights]
