796c4accf691ffe65e696c04c6aece37eb2f3256
Temujin U. Lampasa <temujin.lampasa@obf.ateneo.edu>
2020-10-23 17:19:17 +0800

    Move FlightRow class to its own file

---
+++ src/passenger_view/helpers.py
@@ -0,0 +1,17 @@
+# Helpers ---
+class FlightRow:
+    """A single flight row in the flight_select_view table."""
+
+    def __init__(self, data):
+        if len(data) != 7:
+            raise Exception("Invalid flight data.")
+        self.flight_code = data[0]
+        self.airport_origin = data[1]
+        self.airport_destination = data[2]
+        self.flight_dep_date = data[3]
+        self.flight_arrival_date = data[4]
+        self.flight_duration = data[5]
+        self.flight_cost = data[6]
+
+    # def get_absolute_url(self):
+    #     return
