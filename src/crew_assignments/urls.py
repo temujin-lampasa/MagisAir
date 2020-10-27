0259a4ff29994f01c9c2c3fe30f18047f6439b6c
Temujin U. Lampasa <temujin.lampasa@obf.ateneo.edu>
2020-10-27 15:32:48 +0800

    Add crew detail

--- src/crew_assignments/urls.py
+++ src/crew_assignments/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'crew_assignments'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='crew_home'),
+    path('crew/<int:crew_id>', views.CrewDetail.as_view(), name='crew_detail'),
 ]
--- src/crew_assignments/views.py
+++ src/crew_assignments/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views import View
-import passenger_view.models as models
+from passenger_view.models import (
+    Crew,
+    ScheduledFlight
+)
+from django.views.generic import DetailView
 
 
 def index(request):
@@ -10,9 +14,18 @@ def index(request):
 
 class HomeView(View):
     template_name = 'crew_assignments/home_view.html'
-    crew_list = models.Crew.objects.all()
-    flight_list = models.ScheduledFlight.objects.all()
+    crew_list = Crew.objects.all()
+    flight_list = ScheduledFlight.objects.all()
     context = {'crew_list': crew_list, 'flight_list': flight_list}
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, self.context)
+
+
+class CrewDetail(DetailView):
+    model = Crew
+    template_name = 'crew_assignments/crew_detail.html'
+
+    def get_object(self):
+        id_ = self.kwargs.get("crew_id")
+        return get_object_or_404(Crew, crew_id=id_)
